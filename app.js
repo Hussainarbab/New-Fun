7a5cde2188cb671804077303cdb62ec4f0f7ce0f
Hussainarbab <arbabhussan63@gmail.com>
2024-11-20 16:40:14 +0500

    com

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-// function add(){
-// let inputs =document.getElementById("inputs");
-
-// let inputes=inputs.value;
-
-
-// document.getElementById("show").innerHTML +=inputes+`<button onclick=this.parentElement.remove()> delete<button/> ` + <button onclick="editItem(this)">Edit</button>;
-
-// inputs.value=""
-
-
-// }
-
-
 
 function add() {
     let inputs = document.getElementById("inputs");
     let inputes = inputs.value;
 
-    if (inputes !== "") {
-        // Add a new item with delete and edit buttons
+   
+       
         document.getElementById("show").innerHTML += `
             <div>
                 ${inputes}
@@ -27,19 +13,17 @@ function add() {
             </div>
         `;
 
-        // Clear the input field
         inputs.value = "";
-    }
+    
 }
 
 function editItem(button) {
     let inputs = document.getElementById("inputs");
     let item = button.parentElement;
 
-    // Populate the input field with the current item's text
     inputs.value = item.firstChild.textContent.trim();
 
-    // Remove the current item
+   
     item.remove();
 }
 
